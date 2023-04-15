e7bc67660e6cb4b5e5d10e2baa01ad6a692caad9
Kyle Español <kylekce@gmail.com>
2023-04-15 03:45:08 +0000

    Apr 15, 2023, 3:45 AM

--- Project 1/wiki/encyclopedia/views.py
+++ Project 1/wiki/encyclopedia/views.py
@@ -4,6 +4,8 @@ from . import util
 
 
 def index(request):
+    entries = util.list_entries()
+    css_file = util.get_entry("CSS")
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
