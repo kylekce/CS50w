82a328c811c72bcfdc758573714232ac137a9bdc
Kyle Español <kylekce@gmail.com>
2023-07-21 06:37:08 +0000

    Jul 21, 2023, 6:37 AM

--- Project 2/commerce/auctions/admin.py
+++ Project 2/commerce/auctions/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Category, Listing, Bid, Comment
+from .models import User, Category, Listing
 
 # Register your models here.
 admin.site.register(User)
