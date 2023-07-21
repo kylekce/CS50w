f6227e189569fe2c51cd13129adfccde5b93d67d
Kyle Español <kylekce@gmail.com>
2023-07-21 06:43:34 +0000

    Jul 21, 2023, 6:43 AM

--- Project 2/commerce/auctions/models.py
+++ Project 2/commerce/auctions/models.py
@@ -8,6 +8,9 @@ class User(AbstractUser):
 class Category(models.Model):
     name = models.CharField(max_length=64)
 
+    def __str__(self):
+        return f"{self.name}"
+
 class Listing(models.Model):
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=256)
