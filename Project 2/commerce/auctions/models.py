3ee1f80bf7723259fe9fca2c3ccf1ea365979405
Kyle Español <kylekce@gmail.com>
2023-07-21 06:36:53 +0000

    Jul 21, 2023, 6:36 AM

--- Project 2/commerce/auctions/models.py
+++ Project 2/commerce/auctions/models.py
@@ -4,3 +4,18 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    image = models.CharField(max_length=1000)
+    price = models.FloatField()
+    is_active = models.BooleanField(default=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="user") 
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category") 
