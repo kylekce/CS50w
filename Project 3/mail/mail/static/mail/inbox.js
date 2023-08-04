efd24fb4620ecfa182f058dc42164828faf8fccd
kylekce <kylekce@gmail.com>
2023-08-04 06:42:29 +0000

    Aug 4, 2023, 2:42 PM

--- Project 3/mail/mail/static/mail/inbox.js
+++ Project 3/mail/mail/static/mail/inbox.js
@@ -79,6 +79,7 @@ function load_mailbox(mailbox) {
                 // Add classes to the div
                 emailDiv.classList.add("email");
                 emailDiv.classList.add("border");
+                emailDiv.classList.add("border-dark");
                 emailDiv.classList.add("p-1");
                 // emailDiv.classList.add("mb-1");
 
