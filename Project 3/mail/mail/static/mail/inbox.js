6701d9a09a42d61ce364cc83c538ca16ae506ea5
kylekce <kylekce@gmail.com>
2023-08-04 05:34:21 +0000

    Aug 4, 2023, 1:34 PM

--- Project 3/mail/mail/static/mail/inbox.js
+++ Project 3/mail/mail/static/mail/inbox.js
@@ -103,7 +103,7 @@ function load_mailbox(mailbox) {
                 // If the email is read, add the class to the div
                 if (!email.read) {
                     emailDiv.classList.add("bg-secondary");
-                    emailDiv.classList.add("text-white");
+                    emailDiv.classList.add("text-light");
                 } else {
                     emailDiv.classList.add("read");
                     emailDiv.classList.remove("bg-secondary");
