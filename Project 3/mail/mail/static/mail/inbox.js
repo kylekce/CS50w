ead065281c22134bb3ac3aaa4f34c2761465a0af
kylekce <kylekce@gmail.com>
2023-08-04 04:49:23 +0000

    Aug 4, 2023, 12:49 PM

--- Project 3/mail/mail/static/mail/inbox.js
+++ Project 3/mail/mail/static/mail/inbox.js
@@ -34,20 +34,20 @@ function send_email() {
     const subject = document.querySelector("#compose-subject").value;
     const body = document.querySelector("#compose-body").value;
 
-    // Send the email
-    fetch("/emails", {
-        method: "POST",
-        body: JSON.stringify({
-            recipients: recipients,
-            subject: subject,
-            body: body,
-        }),
-    })
-        .then((response) => response.json())
-        .then((result) => {
-            // Print result
-            console.log(result);
-        });
+    // // Send the email
+    // fetch("/emails", {
+    //     method: "POST",
+    //     body: JSON.stringify({
+    //         recipients: recipients,
+    //         subject: subject,
+    //         body: body,
+    //     }),
+    // })
+    //     .then((response) => response.json())
+    //     .then((result) => {
+    //         // Print result
+    //         console.log(result);
+    //     });
 }
 
 function load_mailbox(mailbox) {
