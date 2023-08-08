f742687bd40fcb7c3e5473e453d0ada87c011f43
kylekce <kylekce@gmail.com>
2023-08-08 23:38:38 +0000

    Aug 9, 2023, 7:38 AM

--- Project 4/project4/network/models.py
+++ Project 4/project4/network/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+    date = models.DateTimeField(auto_now_add=True)
+
+    def str (self):
+        return f"{self.user} posted {self.content} on {self.date}"
