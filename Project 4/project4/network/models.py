9bdc8f685b38ba2a95122aa29723f39f33457c8b
kylekce <kylekce@gmail.com>
2023-08-08 23:44:59 +0000

    Aug 9, 2023, 7:44 AM

--- Project 4/project4/network/models.py
+++ Project 4/project4/network/models.py
@@ -10,5 +10,5 @@ class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
     date = models.DateTimeField(auto_now_add=True)
 
-    def str(self):
+    def __str__(self):
         return f"{self.user} posted {self.content} on {self.date}"
